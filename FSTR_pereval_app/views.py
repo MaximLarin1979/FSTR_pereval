e6bba8c609da3a617431fe39d825804361da1940
Maxim_Larin <maks.larin@gmail.com>
2024-02-05 18:04:54 +0300

    Начал выполнять Спринт 2

--- FSTR_pereval_app/views.py
+++ FSTR_pereval_app/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from rest_framework import generics
+from rest_framework import generics, viewsets, mixins
 from .serializers import *
 
 
@@ -20,3 +20,29 @@ class PerevalAddedAPI(generics.CreateAPIView):
         except Exception as exc:
             responsedata = {'status': '400', 'message': f'Bad Request: {exc}', 'id': 'null'}
             return JsonResponse(responsedata, status=400, safe=False)
+
+
+class PerevalDetailAPI(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
+    # класс для получения детальных данных по перевалу и для редактирования этих данных (если status=NEW)
+    queryset = PerevalAdded.objects.all()
+    serializer_class = PerevalDetailSerializer
+
+    def update(self, request, *args, **kwargs):
+        """
+        Переопределение метода update(PATCH)
+        """
+        pk = kwargs.get("pk", None)
+
+        try:
+            instance = PerevalAdd.objects.get(pk=pk)
+        except:
+            return Response({"error": "Такого перевала не существует"}, status=400)
+
+        if instance.status != "N":
+            return Response({"message": "Перевал на модерации, вы не можете его изменить",
+                             "state": 0}, status=400)
+        else:
+            serializer = PerevalDetailSerializer(data=request.data, instance=instance)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({"state": 1}, status=200)
